8cdc937d83202848c3909f74ee458e0eeedea972
Unknown <jgmalafronte@gmail.com>
2018-05-09 16:59:47 -0400

    Finish First Parse

--- parser.py
+++ parser.py
@@ -14,5 +14,29 @@ uClient.close()
 page_soup = soup(page_html, "html.parser") 
 
 
+filename = "products.csv"
+f = open(filename, "w")
+
+headers = "brand, productName, shipping\n"
+
+f.write(headers)
+
+
 #grabs each product places into array
 containers = page_soup.findAll("div", {"class","item-container"})
+
+for container in containers:
+	brand = container.div.div.a.img["title"]
+	title_container = container.findAll("a", {"class","item-title"})
+	productName = title_container[0].text.strip()
+	shippingPriceC = container.findAll("li", {"class","price-ship"})
+	shippingPrice = shippingPriceC[0].text.strip()
+
+	print("Brand: " + brand)
+	print("Product Name: " + productName)
+	print("Shipping Price" + shippingPrice)
+
+	f.write(brand + "," + productName.replace(",", "|") + "," + shippingPrice +"\n")
+
+
+f.close()
