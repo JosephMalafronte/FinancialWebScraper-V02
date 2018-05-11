f47cddb68fc5ad421a748387558447916dd39e58
Unknown <jgmalafronte@gmail.com>
2018-05-11 12:01:14 -0400

    Updated parser.py

--- parser.py
+++ parser.py
@@ -1,5 +1,7 @@
 #Joseph Malafronte
 #Web Scraper 1
+#Simple Web Scraper practice program
+#Pulls graphics card information from Newegg.com
 
 
 from urllib.request import urlopen as uReq
@@ -32,9 +34,20 @@ for container in containers:
 	shippingPriceC = container.findAll("li", {"class","price-ship"})
 	shippingPrice = shippingPriceC[0].text.strip()
 
+	#Product Price
+	infoContainer = container.findAll("div", {"class","item-info"})
+	actionContainer = infoContainer[0].findAll("div", {"class", "item-action"})
+	priceContainer = actionContainer[0].ul
+	lastPriceContainer = priceContainer.findAll("li", {"class","price-current"})
+	price = lastPriceContainer[0].strong.text.strip()
+
+
 	print("Brand: " + brand)
 	print("Product Name: " + productName)
-	print("Shipping Price" + shippingPrice)
+	print("Shipping Price: " + shippingPrice)
+	print("Product Price: $" + price)
+	print("\n")
+
 
 	f.write(brand + "," + productName.replace(",", "|") + "," + shippingPrice +"\n")
 
